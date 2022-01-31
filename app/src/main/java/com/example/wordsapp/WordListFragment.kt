6e2213ac20d697f30a0d67bd466f6377bbae7a28
diskostu <denny.kluge@gmail.com>
2022-01-31 16:49:53 +0100

    finished adding navigation

--- app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -14,16 +14,31 @@ class WordListFragment : Fragment() {
     private var _binding: FragmentWordListBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var letterId: String
+
     /**
      * Provides global access to these variables from anywhere in the app
      * via DetailActivity.<variable> without needing to create
      * a DetailActivity instance.
      */
     companion object {
+        // the value has to correspond with the argument name defined in nav_graph.xml
         const val LETTER = "letter"
         const val SEARCH_PREFIX = "https://www.google.com/search?q="
     }
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+
+//        if (arguments != null) {
+//            letterId = requireArguments().getString(LETTER).toString()
+//        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentWordListBinding.inflate(inflater, container, false)
@@ -34,7 +49,6 @@ class WordListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = binding.recyclerView
 
-        val letterId = activity?.intent?.extras?.getString(LETTER).toString()
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = WordAdapter(letterId, requireContext())
 
